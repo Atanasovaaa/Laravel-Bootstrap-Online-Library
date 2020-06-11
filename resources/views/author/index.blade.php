9d39eaeb77be8e60387c25ae77587a7215085b75
Atanasovaaa <dankkatta@gmail.com>
2020-06-12 02:06:25 +0300

    Add book cards in author view

--- resources/views/author/index.blade.php
+++ resources/views/author/index.blade.php
@@ -3,5 +3,26 @@
 @section('content')
 
 <span>{{$author->name}}</span>
-
+<div class="container">
+    <div class="row row-cols-1 row-cols-md-3">
+        @foreach($author->books as $book)
+        <div class="col mb-4">
+            <div class="card mb-3" style="max-width: 540px;">
+                <div class="row no-gutters">
+                    <div class="col-md-4">
+                        <img src="{{ asset('images/throne=of-glass.jpg') }}" class="card-img" alt="...">
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card-body">
+                            <h5 class="card-title">{{$book->name}}</h5>
+                            <p class="card-text">{{$book->description}}</p>
+                            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        @endforeach
+    </div>
+</div>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,7 +23,7 @@
     <div id="app">
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
             <div class="container">
-                <a class="navbar-brand" href="#">Online Library</a>
+                <a class="navbar-brand" href="/">Online Library</a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
